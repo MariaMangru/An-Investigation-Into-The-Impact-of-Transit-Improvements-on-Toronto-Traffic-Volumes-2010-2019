57fa5545bdae21b6c0cfd0a5c9f995460c23ee64
Maria Mangru <mangrumaria@gmail.com>
2024-04-18 05:32:52 -0400

    completed discussion and model results, updated references and included literature

--- scripts/03-exploring_cleaned_data.R
+++ scripts/03-exploring_cleaned_data.R
@@ -17,12 +17,12 @@ library(here)
 ### Creating Summary Statistics and Visualizations ###
 
 # Load the cleaned data
-cleaned_data <- read_csv(here("data", "analysis_data", "cleaned-data.csv"))
+analysis_data <- read_csv(here("data", "analysis_data", "cleaned-data.csv"))
 
 # Summary Statistics
 
 # Generate and view summary statistics for traffic data
-summary_stats <- cleaned_data %>%
+summary_stats <- analysis_data %>%
   summarise(
     Mean_Cars = mean(daily_cars, na.rm = TRUE),
     Median_Cars = median(daily_cars, na.rm = TRUE),
@@ -39,7 +39,7 @@ View(summary_stats)
 # Visualize Data
 
 # Visualizing daily car traffic over time
-line_graph_car <- cleaned_data %>%
+line_graph_car <- analysis_data %>%
   ggplot(aes(x = count_date, y = daily_cars)) +
   geom_smooth(se = FALSE) +  
   labs(title = "Daily Cars Traffic Over Time",
@@ -54,7 +54,7 @@ line_graph_car
 ggsave("./other/outputs/car_traffic_over_time.png", plot = line_graph_car, width = 12, height = 6, units = "in")
 
 # Visualizing daily bus traffic over time
-line_graph_buses <- cleaned_data %>%
+line_graph_buses <- analysis_data %>%
   ggplot(aes(x = count_date, y = daily_bus)) +
   geom_smooth(se = FALSE) +
   labs(title = "Daily Bus Traffic Over Time",
@@ -69,7 +69,7 @@ line_graph_buses
 ggsave("./other/outputs/bus_traffic_over_time.png", plot = line_graph_buses, width = 12, height = 6, units = "in")
 
 # Visualizing daily pedestrian traffic over time
-line_graph_pedestrians <- cleaned_data %>%
+line_graph_pedestrians <- analysis_data %>%
   ggplot(aes(x = count_date, y = daily_peds)) +
   geom_smooth(se = FALSE) +
   labs(title = "Daily Pedestrian Traffic Over Time",
@@ -84,7 +84,7 @@ line_graph_pedestrians
 ggsave("./other/outputs/pedestrian_traffic_over_time.png", plot = line_graph_pedestrians, width = 12, height = 6, units = "in")
 
 # Visualizing daily bike traffic over time
-line_graph_cyclists <- cleaned_data %>%
+line_graph_cyclists <- analysis_data %>%
   ggplot(aes(x = count_date, y = daily_bike)) +
   geom_smooth(se = FALSE) +
   labs(title = "Daily Cyclist Traffic Over Time",
@@ -101,7 +101,7 @@ ggsave("./other/outputs/cyclist_traffic_over_time.png", plot = line_graph_cyclis
 
 
 # Visualizing car traffic with respect to the time trend
-time_trend_graph_car <- cleaned_data %>%
+time_trend_graph_car <- analysis_data %>%
   ggplot(aes(x = time_trend, y = daily_cars)) +
   geom_point(aes(color = daily_cars), alpha = 0.5) + 
   geom_smooth(method = "lm", se = FALSE, color = "blue") +  
